962caaa371241fe6e8921309b7b9e659486fd359
napee19 <napee1904@gmail.com>
2022-07-26 13:26:39 -0700

    mid CP3 & CP4

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -21,6 +21,7 @@ contract Staker {
   event Stake(address _staker, uint256 _amount);
   
   function stake() public payable{
+    require(block.timestamp < deadline, "Staking is closed");
     balances[msg.sender] += msg.value;
     emit Stake(msg.sender, msg.value);
   }
@@ -28,21 +29,30 @@ contract Staker {
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
   uint256 public deadline = block.timestamp + 30 seconds;
+  bool openForWithdraw;
   
-  function execute() public {
-    if (balances[msg.sender] >= threshold && block.timestamp >= deadline) {
+  function execute() public notCompleted {
+    if (balances[msg.sender] >= threshold) {
       exampleExternalContract.complete{value: balances[msg.sender]}();
+    } else {
+      openForWithdraw = true;
     }
-    // If the `threshold` was not met, allow everyone to call a `withdraw()` function
-    if (balances[msg.sender] < threshold) {
-      bool openForWithdraw = true;
-    }
-    if (block.timestamp >= deadline && openForWithdraw) {
-      withdraw();
-    }
+  }
 
+  // If the `threshold` was not met, allow everyone to call a `withdraw()` function
+  modifier notCompleted() {
+    require(block.timestamp >= deadline, "Action is available after deadline");
+    require(exampleExternalContract.complete == false, "Staking process is complete, Action is no longer available");
+    _;
+  }
+    
   // Add a `withdraw()` function to let users withdraw their balance
-  function withdraw() public {
+  function withdraw() public notCompleted {
+    require(balances[msg.sender] < threshold, "Threshold is met, withdraw is not available");
+    require(openForWithdraw, "Withdraw is not available");
+    require(balances[msg.sender] > 0, "No funds to withdraw");
+    
+    openForWithdraw = true;
     balances[msg.sender] = 0;
   }
 
