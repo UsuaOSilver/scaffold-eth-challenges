13330e8eea2c8da363201128bed42a9270158d47
napee19 <napee1904@gmail.com>
2022-07-26 11:06:50 -0700

    CP3 - Goals met

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -27,17 +27,33 @@ contract Staker {
     
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
-
-
-  // If the `threshold` was not met, allow everyone to call a `withdraw()` function
-
+  uint256 public deadline = block.timestamp + 30 seconds;
+  
+  function execute() public {
+    if (balances[msg.sender] >= threshold && block.timestamp >= deadline) {
+      exampleExternalContract.complete{value: balances[msg.sender]}();
+    }
+    // If the `threshold` was not met, allow everyone to call a `withdraw()` function
+    if (balances[msg.sender] < threshold) {
+      bool openForWithdraw = true;
+    }
+    if (block.timestamp >= deadline && openForWithdraw) {
+      withdraw();
+    }
 
   // Add a `withdraw()` function to let users withdraw their balance
-
+  function withdraw() public {
+    balances[msg.sender] = 0;
+  }
 
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    }
+    return deadline - block.timestamp;
+  }
 
   // Add the `receive()` special function that receives eth and calls stake()
   receive() external payable {
