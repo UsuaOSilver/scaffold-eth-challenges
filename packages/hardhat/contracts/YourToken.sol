7618391e112f6f5af8e016f6225c6d6e060d056c
napee19 <napee1904@gmail.com>
2022-08-12 13:53:16 -0700

    Checkpoint 2

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
-    constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+    constructor() ERC20("Sou", "SOU") {
+        _mint(0xaf613770cB500BdAb0ee5d8Cefe9D701C88D6486, 1000 * 10 ** 18);
     }
 }
